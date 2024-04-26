3e7b2cafad225aac1ed25f6987af4dd1666e1367
bojevilya <igor.nedozdhii@gmail.com>
2024-04-26 23:00:33 +0300

    rename

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from datetime import datetime, date, timedelta
+from datetime import datetime
 
 
 def get_days_from_today(date):
     try:
-        choosen_date = datetime.strptime(date, '%Y-%m-%d')
+        chosen_date = datetime.strptime(date, '%Y-%m-%d')
         today = datetime.today()
-        delta = today - choosen_date
+        delta = today - chosen_date
         return delta.days
     except ValueError:
         return ('Wrong format. Use "YYYY-MM-DD"')
 
 
-choosen_date = ("2024-03-26")
-print(get_days_from_today(choosen_date))
+date = "2024-03-26"
+print(get_days_from_today(date))
